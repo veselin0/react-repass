02063ccc81ddcade1dac401740042ad2361c61eb
Veselin Velkov <vesselin60@gmail.com>
2025-10-14 16:24:09 +0300

    Implicit return in an arrow function

--- tutorial-03-component/src/components/Instructions/Instructions.js
+++ tutorial-03-component/src/components/Instructions/Instructions.js
@@ -2,13 +2,12 @@ import React from "react";
 import "./Instructions.css";
 import logo from "./logo.svg";
 
-const Instructions = () => {
-	return (
+const Instructions = () => (
 		<div className="instructions">
 			<img alt="laughing crying emoji" src={logo} />
 			<p>Click on an emoji to view the emoji short name.</p>
 		</div>
 	);
-};
+
 
 export default Instructions;
