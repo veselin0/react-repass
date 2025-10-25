44bc0e4874c886a9a5de780f498fe320680e9c0e
Veselin Velkov <vesselin60@gmail.com>
2025-10-25 16:41:53 +0300

    176 170 Step 2 Part 1

--- prop-tutorial/src/components/AnimalCard/AnimalCard.js
+++ prop-tutorial/src/components/AnimalCard/AnimalCard.js
@@ -1,20 +1,22 @@
 import React from "react";
-// import "./AnimalCard.scc ";
+import "./AnimalCard.css";
 
 export default function AnimalCard({
 	additional,
 	diet,
 	name,
 	scientificName,
+	showAdditionalData,
 	size,
 }) {
 	// const { name } = props;
 	return (
-		<div>
+		<div className="animal-wrapper">
 			<h2>{name}</h2>
 			<h3>{scientificName}</h3>
 			<h4>{size}kg</h4>
 			<div>{diet.join(", ")}.</div>
+			<button onClick={()=>showAdditionalData(additional)}>More information</button>
 		</div>
 	);
 }
--- prop-tutorial/src/components/App/App.js
+++ prop-tutorial/src/components/App/App.js
@@ -3,6 +3,13 @@ import data from "./data";
 import AnimalCard from "../AnimalCard/AnimalCard";
 import "./App.css";
 
+function showAdditionalData(additional) {
+	const alertInformation = Object.entries(additional)
+		.map((information) => `${information[0]}: ${information[1]}`)
+		.join("\n");
+	alert(alertInformation);
+}
+
 function App() {
 	return (
 		<div className="wrapper">
@@ -14,6 +21,7 @@ function App() {
 					additional={animal.additional}
 					diet={animal.diet}
 					scientificName={animal.scientificName}
+					showAdditionalData={showAdditionalData}
 					size={animal.size}
 				/>
 			))}
